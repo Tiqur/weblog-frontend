439e2137f7daa6b7ad3cf3b19911cdd445b8f624
Tiqur <Tiqur7@gmail.com>
2022-08-04 16:17:59 -0700

    Added infinite scrolling

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,4 +1,6 @@
 import styles from './styles.module.scss';
+import InfiniteScroll from "react-infinite-scroll-component";
+import {useState} from 'react';
 
 const AddTradeButton = () => {
   return (
@@ -22,25 +24,42 @@ function generateFakeTrade() {
 
 
 function HomePage() {
-  const trades = [];
-  for (let i=0; i<20; i++) {
+  const [trades, setTrades] = useState([])
+
+
+  for (let i=0; i<50; i++) {
     trades.push(generateFakeTrade())
   }
 
+  function fetchMoreData() {
+    setTimeout(() => {
+      const new_trades = [];
+      for (let i=0; i<50; i++)
+        new_trades.push(generateFakeTrade())
+
+      setTrades([...trades, ...new_trades])
+    }, 1000);
+  };
+
   return (
     <div classname={styles.container}>
-      <div className={styles.trades_container}>
-        {trades.map(e => (
-          <div className={styles.trade_log}>
-            <p>{e.time_entry}</p>
-            <p>{e.time_exit}</p>
-            <p>{e.position_type}</p>
-            <p>${e.entry_price}</p>
-            <p>${e.exit_price}</p>
-            <p>%{e.entry_price / e.entry_price}</p>
-          </div>
-        ))}
-      </div>
+        <InfiniteScroll 
+          className={styles.trades_container}
+          dataLength={trades.length}
+          next={fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}>
+          {trades.map(e => (
+            <div className={styles.trade_log}>
+              <p>{e.time_entry}</p>
+              <p>{e.time_exit}</p>
+              <p>{e.position_type}</p>
+              <p>${e.entry_price}</p>
+              <p>${e.exit_price}</p>
+              <p>%{e.entry_price / e.entry_price}</p>
+            </div>
+          ))}
+        </InfiniteScroll>
       <AddTradeButton/>
     </div>
   );
