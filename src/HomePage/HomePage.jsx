469efabd5e37cc1608f3e1182b8ebde6198a86d4
Tiqur <Tiqur7@gmail.com>
2022-08-09 14:49:42 -0700

    Styled bar chart and fixed data insertion

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,21 +1,21 @@
 import styles from './styles.module.scss';
 import InfiniteScroll from "react-infinite-scroll-component";
 import {useState, useEffect} from 'react';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title } from 'chart.js';
+import { Pie, Bar } from 'react-chartjs-2';
 
-const AddTradeButton = () => {
+const AddTradeButton = (trades) => {
   return (
     <div onClick={() => {
-      console.log("test")
+      console.log(trades)
     }} className={styles.add_trade_button}>
 
     </div>
   )
 }
 
-function calculatePL(position_type, entry_price, exit_price) {
-  return (position_type == 'LONG' ? (exit_price-entry_price)/entry_price : (entry_price-exit_price)/exit_price);
+function calculatePL(e) {
+  return (e.position_type == 'LONG' ? (e.exit_price-e.entry_price)/e.entry_price : (e.entry_price-e.exit_price)/e.exit_price);
 }
 
 function formatUnixToDate(unix) {
@@ -35,9 +35,11 @@ function generateFakeTrade() {
 
 
 function HomePage() {
-  const [trades, setTrades] = useState([])
-  const [windowHeight, setWindowHeight] = useState(window.innerHeight)
-  ChartJS.register(ArcElement, Tooltip, Legend);
+  const [trades, setTrades] = useState([]);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  const [data1, setData1] = useState({datasets:[]});
+  const [data2, setData2] = useState({datasets:[]});
+  ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title);
 
   // Update window height state on resize
   useEffect(() => {
@@ -46,7 +48,41 @@ function HomePage() {
     });
   }, []);
 
+  // Update trades_pl array on trades update
+  useEffect(() => {
+    const trades_pl = trades.map(e => calculatePL(e));
+    console.log(trades, trades_pl)
+      
+    setData1({
+      labels: ['Win', 'Loss'],
+      datasets: [
+            {
+              label: 'P/L',
+              data: [trades_pl.filter(e => e >= 0).length / trades_pl.length, trades_pl.filter(e => e < 0).length / trades_pl.length],
+              backgroundColor: [
+                'rgba(122, 179, 199, 0.8)',
+                'rgba(48, 151, 178, 0.8)'
+              ],
+              borderColor: [
+                'rgba(122, 179, 199, 0.8)',
+                'rgba(48, 151, 178, 0.8)'
+              ],
+              borderWidth: 1,
+            },
+          ],
+        })
 
+    setData2({
+      labels: trades.map((e, index) => index),
+      datasets: [
+        {
+          label: 'Trades',
+          data: trades_pl,
+          backgroundColor: 'rgba(122, 179, 199, 0.8)',
+        }
+      ]
+    })
+  }, [trades]);
 
 
   // Gen fake trades
@@ -65,32 +101,15 @@ function HomePage() {
   };
 
 
-  // Temp
-  const data = {
-    labels: ['Win', 'Loss'],
-    datasets: [
-          {
-            label: '# of Votes',
-            data: [62, 38],
-            backgroundColor: [
-              'rgba(122, 179, 199, 0.8)',
-              'rgba(48, 151, 178, 0.8)'
-            ],
-            borderColor: [
-              'rgba(122, 179, 199, 0.8)',
-              'rgba(48, 151, 178, 0.8)'
-            ],
-            borderWidth: 1,
-          },
-        ],
-      }
-
-
-
   return (
     <div className={styles.container}>
         <div className={styles.stats_container} style={{height: window.innerHeight*0.3}}>
-          <Pie options={{ maintainAspectRatio: false }} data={data}/>
+          <div className={styles.pie_chart_container}>
+            <Pie options={{ maintainAspectRatio: false }} data={data1}/>
+          </div>
+          <div className={styles.bar_chart_container}>
+            <Bar options={{ maintainAspectRatio: false }} data={data2}/>
+          </div>
         </div>
         <table className={styles.trade_log_container}>
           <tr className={styles.trade_log_label}>
@@ -116,12 +135,12 @@ function HomePage() {
                   <td style={{color: e.position_type == 'LONG' ? '#62C951' : '#D15D5D'}}>{e.position_type}</td>
                   <td>${e.entry_price}</td>
                   <td>${e.exit_price}</td>
-                  <td style={{color: calculatePL(e.position_type, e.entry_price, e.exit_price)>=0 ? '#62C951' : '#D15D5D'}}>%{calculatePL(e.position_type, e.entry_price, e.exit_price).toFixed(2)}</td>
+                  <td style={{color: calculatePL(e)>=0 ? '#62C951' : '#D15D5D'}}>%{calculatePL(e).toFixed(2)}</td>
                 </tr>
             ))}
           </table>
         </InfiniteScroll>
-      <AddTradeButton/>
+      <AddTradeButton trades={[trades]}/>
     </div>
   );
 }
